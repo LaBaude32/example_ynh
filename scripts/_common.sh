841dabac9c8947186568ce1ddcae8a6c08cef9ab
Baudouin Coupey <baudouincoupey@pop-os.localdomain>
2020-12-04 19:52:12 +0100

    first test config

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,6 +5,11 @@
 #=================================================
 
 # dependencies used by the app
+
+YNH_PHP_VERSION="7.3"
+
+YNH_COMPOSER_VERSION="2.0.8"
+
 pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
 
 #=================================================
